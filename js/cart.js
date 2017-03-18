55067250b9dbc055e69f619ecfe7599e8e8ce163
Armandfgs <armandfgs@gmail.com>
2017-03-18 19:12:10 +0100

    Revert "First Upload"
    This reverts commit bc462fbefcfeee5368286dbf4f6a27fd3e62a08d.

--- js/cart.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Admin on 11/02/2017.
- */
-function deleteRow(e)
-{
-    e.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode.parentNode.parentNode);
-}
